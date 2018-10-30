b7251df1cc6d33b0c2413731166da5f691850bcb
Aisling Kathrine <Aisling@Aislings-MacBook-Air.local>
2018-10-30 21:32:02 +0100

    efter lauras ændringer

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-window.addEventListener("load", start);
+window.addEventListener("load", sidenVises);
+
+alert("Connected");
+
+function sidenVises() {
+    console.log("siden vises");
+
+    showStart();
+}
+
+function showStart() {
+    console.log("show start");
+
+    document.querySelector("#start").classList.remove("hide");
+    document.querySelector("#play").classList.add("pulse");
+    document.querySelector("#play").addEventListener("click", hideStart);
+
+}
+
+function hideStart() {
+    console.log("hide start");
+
+    document.querySelector("#play").removeEventListener("click", hideStart);
+    document.querySelector("#play").classList.remove("pulse");
+    document.querySelector("#start").classList.add("fade_out");
+    document.querySelector("#start").addEventListener("animationend", startGame);
+}
+
+function startGame() {
+    console.log("start game");
+
+    document.querySelector("#start").removeEventListener("animationend", startGame);
+    document.querySelector("#start").classList.remove("fade_out");
+    document.querySelector("#start").classList.add("hide");
+}
